2dfa6bfc9bc12c55a6565b0dfae147104c041078
Awe Some <foundationawesome@gmailcom>
2014-11-23 20:24:00 +0100

    updated networks config

--- networks.js
+++ networks.js
@@ -7,15 +7,14 @@ var hex = function(hex) {
 exports.livenet = {
   name: 'livenet',
   magic: hex('71362306'),
-  addressVersion: 0x23,
-  privKeyVersion: 191,
+  addressVersion: 23,
+  privKeyVersion: 151,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    //hash: hex('0x00005ce0afa0524504139e33db06026767ade60e65e2bc3913bfa176d7163333'),
-    hash: hex('A1F507EB29B380187722CCDA4EB4B84704B31956C40F36E48C53C1'),
-    merkle_root: hex('0x0000724595fb3b9609d441cbfb9577615c292abf07d996d3edabc48de843642d'),
+    hash: hex('333316d776a1bf1339bce2650ee6ad67670206db339e13044552a0afe05c0000'),
+    merkle_root: hex('830a822ca687f35a66acb7fb993b63373859d6eac5cae348c75ff2bcadc7b6e8'),
     height: 0,
     nonce: 36561,
     version: 1,
@@ -28,36 +27,6 @@ exports.livenet = {
   ],
   defaultClientPort: 61224
 };
-
-/*{
-  name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
-  privKeyVersion: 128,
-  P2SHVersion: 5,
-  hkeyPublicVersion: 0x0488b21e,
-  hkeyPrivateVersion: 0x0488ade4,
-  genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
-    height: 0,
-    nonce: 2083236893,
-    version: 1,
-    prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    bits: 486604799,
-  },
-  dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
-  ],
-  defaultClientPort: 8333
-};
-*/
 
 exports.mainnet = exports.livenet;
 
